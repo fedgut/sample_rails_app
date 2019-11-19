c57c315e349af3176d234a40e8d87b5a26eba9a4
fedgut <fedgut@gmail.com>
2019-11-19 09:35:08 -0600

    Add additional chapter 10 elems

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -15,9 +17,29 @@ class UsersController < ApplicationController
       log_in @user
       flash[:success] = 'Welcome to the Sample App!'
       redirect_to @user
-      # Handle a successful save.
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = user.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = 'Please log in'
+      redirect_to login_url
     end
   end
 
